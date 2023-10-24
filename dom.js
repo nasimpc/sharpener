c951a4aa9b1403acbabf300611d6db12dc49321d
Muhammed Nasim PC <100104479+nasimpc@users.noreply.github.com>
2023-10-24 17:08:26 +0530

    sharpenerdom

---
+++ dom.js
@@ -0,0 +1,24 @@
+// console.dir (document) ;
+// console.log(document.domain) ;
+// console.log(document.URL);
+// console.log(document.title);
+////document.title
+
+// console.log(document. doctype);
+// console.log(document.head) ;
+// console.log(document.body) ;
+// console.log (document.all);
+// console.log(document.all[10]);
+// // document.all[10]. textcontent = 'Hello';
+// console.log(document. forms[0]);
+// console.log(document.links);
+// console. log (document.images);
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerText = 'Goodbye';
+headerTitle.style.borderBottom = 'solid 3px #000'
+var addItem1 = document.getElementById('additems1');
+addItem1.style.color = 'green';
+addItem1.style.fontWeight = 'bold'; 
